f23d02a9c32f86e4cbb78a76c243c25cc29865cb
luke-windows <luke.zhu@thinkchina.com>
2021-08-26 23:45:02 +1000

    io

--- src/io/index.js
+++ src/io/index.js
@@ -9,6 +9,21 @@ const writeStream = fs.createWriteStream(file2);
 
 readStream.pipe(writeStream);
 
+readStream.on("on", function (chunk) {
+  console.log("chunk", chunk);
+});
+
 readStream.on("end", function () {
   console.log("finish");
+});
+
+//get download
+const http = require("http");
+const server = http.createServer(function (req, res) {
+  const method = req.method;
+  if (method === "GET") {
+    const fileName = fs.createReadStream(file1);
+    const stream = fs.createReadStream(fileName);
+    stream.pipe(res);
+  }
 });
